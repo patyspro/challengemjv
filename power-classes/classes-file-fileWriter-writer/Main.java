abf8b4957dbba50a410fac3329de78ff2af03d1d
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2021-12-26 18:51:56 -0300

    Testando classes relatório arquivos e modificando arquivos

---
+++ power-classes/classes-file-fileWriter-writer/Main.java
@@ -0,0 +1,16 @@
+package desafio;
+
+import java.io.File;
+
+public class Main {
+
+	public static void main(String[] args) {
+		File arq = new File("D:\\PROJETOS\\desafio\\src\\desafio\\texto.txt");
+		File dir = new File("D:\\PROJETOS\\desafio\\src\\desafio");
+		
+		RelatorioArquivo.exibirRelatorio(arq);
+		
+		ModificaArquivoTexto.modificar(arq);
+//		ModificaArquivoTexto.inserirLinha(arq);
+	}
+}
